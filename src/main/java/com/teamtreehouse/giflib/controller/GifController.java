4772c4a4bb7cb67a147d665b94730d59d2f5b5b7
Geoffrey Emerson <Geoffrey.Emerson.git@gmail.com>
2017-04-17 22:11:42 -0700

    add controller entry for gif-detail

--- src/main/java/com/teamtreehouse/giflib/controller/GifController.java
+++ src/main/java/com/teamtreehouse/giflib/controller/GifController.java
@@ -1,8 +1,11 @@
 package com.teamtreehouse.giflib.controller;
 
+import com.teamtreehouse.giflib.model.Gif;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.time.LocalDate;
 
 @Controller
 public class GifController {
@@ -10,6 +13,13 @@ public class GifController {
     @RequestMapping("/")
     public String listGifs() {
         return "home"; // used by thymeleaf view resolver to find html file
+    }
+
+    @RequestMapping("/gif")
+    public String gifDetails(ModelMap modelMap) {
+        Gif gif = new Gif("compiler-bot", LocalDate.of(2015,2,13), "Chris Ramacciotti", true);
+        modelMap.put("gif",gif);
+        return "gif-details";
     }
 
 }
