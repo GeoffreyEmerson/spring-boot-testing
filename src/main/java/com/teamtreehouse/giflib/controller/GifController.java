b432b7875a930ef0a6ef439f9988455c6d34a744
Geoffrey Emerson <geoffrey.emerson.git@gmail.com>
2017-04-17 23:56:05 -0700

    rempove extraneous import

--- src/main/java/com/teamtreehouse/giflib/controller/GifController.java
+++ src/main/java/com/teamtreehouse/giflib/controller/GifController.java
@@ -7,8 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.time.LocalDate;
-
 @Controller
 public class GifController {
 
